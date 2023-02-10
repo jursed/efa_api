d993e2b0886ee8873f1b5c7ec139cab1f81427ce
sedivjur <jursed@gmail.com>
2023-02-10 08:30:29 +0100

    Black reformat

--- efa_api/conn.py
+++ efa_api/conn.py
@@ -2,5 +2,5 @@ import connexion
 from connexion.resolver import RelativeResolver
 
 app = connexion.FlaskApp(__name__)
-app.add_api('openapi.yaml', resolver=RelativeResolver("efa_api"))
+app.add_api("openapi.yaml", resolver=RelativeResolver("efa_api"))
 app.run(port=8080)
